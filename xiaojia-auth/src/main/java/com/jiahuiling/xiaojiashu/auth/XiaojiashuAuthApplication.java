a429e2a1472f41a18e6e45289da95fa0e0d900f5
3040016652 <3040016652@qq.com>
2026-09-03 11:29:10 +0800

    整合 MyBatis 持久层框架以及 Druid 数据库连接池

--- xiaojia-auth/src/main/java/com/jiahuiling/xiaojiashu/auth/XiaojiashuAuthApplication.java
+++ xiaojia-auth/src/main/java/com/jiahuiling/xiaojiashu/auth/XiaojiashuAuthApplication.java
@@ -1,9 +1,11 @@
-package com.jiahuiling.framework.common.xiaojiashu.auth;
+package com.jiahuiling.xiaojiashu.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.jiahuiling.xiaojiashu.auth.domain.mapper")
 public class XiaojiashuAuthApplication {
 
     public static void main(String[] args) {
