53b409873d91bf47cf1283190cebaa21ae6212ed
3040016652 <3040016652@qq.com>
2026-09-04 21:03:07 +0800

    Guava 参数校验，同步角色权限到redis中

--- xiaojia-auth/src/main/java/com/jiahuiling/xiaojiashu/auth/domain/mapper/RoleDOMapper.java
+++ xiaojia-auth/src/main/java/com/jiahuiling/xiaojiashu/auth/domain/mapper/RoleDOMapper.java
@@ -2,6 +2,8 @@ package com.jiahuiling.xiaojiashu.auth.domain.mapper;
 
 import com.jiahuiling.xiaojiashu.auth.domain.dataobject.RoleDO;
 
+import java.util.List;
+
 public interface RoleDOMapper {
     int deleteByPrimaryKey(Long id);
 
@@ -14,4 +16,6 @@ public interface RoleDOMapper {
     int updateByPrimaryKeySelective(RoleDO record);
 
     int updateByPrimaryKey(RoleDO record);
+
+    List<RoleDO> selectEnabledList();
 }
