375f863fe6e72f7d11c29fc0b0a7fa630bb49553
3040016652 <3040016652@qq.com>
2026-09-05 17:51:01 +0800

    auth过滤器+threadLocal实现上下文传递，以及修改密码功能

---
+++ xiaojia-auth/src/main/java/com/jiahuiling/xiaojiashu/auth/model/VO/user/UpdatePasswordReqVO.java
@@ -0,0 +1,23 @@
+package com.jiahuiling.xiaojiashu.auth.model.VO.user;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author 贾慧玲
+ * @Date 2026/9/5 17:10
+ * @Description 修改用户密码
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdatePasswordReqVO {
+
+    @NotBlank(message = "新密码不能为空")
+    private String newPassword;
+
+}
---
+++ xiaojia-framework/xiaojia-common/src/main/java/com/jiahuiling/framework/common/constant/GlobalConstants.java
@@ -0,0 +1,14 @@
+package com.jiahuiling.framework.common.constant;
+
+/**
+ * @Author 贾慧玲
+ * @Date 2026/9/5 16:34
+ * @Description TODO
+ */
+public interface GlobalConstants {
+
+    /**
+     * 用户 ID
+     */
+    String USER_ID = "userId";
+}
