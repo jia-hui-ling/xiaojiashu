dd5bdcec7d0ad9ba9f6ac04ddf07806048fee835
贾慧玲 <jhl@915.cn>
2026-09-14 09:40:42 +0800

    分布式id生成服务-整合leaf源码-小哈书id用户id重构调用分布式id服务

---
+++ xiaojiashu-distributed-id-generator/xiaojiashu-distributed-id-generator-api/src/main/java/com/jiahuiling/xiaojiashu/distributed/generator/constant/ApiConstants.java
@@ -0,0 +1,15 @@
+package com.jiahuiling.xiaojiashu.distributed.generator.constant;
+
+/**
+ * @Author 贾慧玲
+ * @Date 2026/9/11 10:59
+ * @description: 常量
+ **/
+public interface ApiConstants {
+
+    /**
+     * 服务名称
+     */
+    String SERVICE_NAME = "xiaojiashu-distributed-id-generator";
+}
+
