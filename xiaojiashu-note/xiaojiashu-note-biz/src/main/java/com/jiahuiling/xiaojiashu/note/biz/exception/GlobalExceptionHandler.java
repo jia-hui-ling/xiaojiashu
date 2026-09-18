077ea72e1bd4435947ec16b9fd1838476c917505
3040016652 <3040016652@qq.com>
2026-09-18 09:51:48 +0800

    笔记服务搭建-2

---
+++ xiaojiashu-note/xiaojiashu-note-biz/src/main/java/com/jiahuiling/xiaojiashu/note/biz/exception/GlobalExceptionHandler.java
@@ -0,0 +1,65 @@
+package com.jiahuiling.xiaojiashu.note.biz.exception;
+
+import com.jiahuiling.framework.common.exception.BizException;
+import com.jiahuiling.framework.common.response.Response;
+import com.jiahuiling.xiaojiashu.note.biz.enums.ResponseCodeEnum;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BizException.class)
+    @ResponseBody
+    public Response<Object> handleBizException(HttpServletRequest request, BizException e) {
+        log.warn("{} request fail, errorCode: {}, errorMessage: {}", request.getRequestURI(), e.getErrorCode(), e.getErrorMessage());
+        return Response.fail(e);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseBody
+    public Response<Object> handleMethodArgumentNotValidException(HttpServletRequest request, MethodArgumentNotValidException e) {
+        String errorCode = ResponseCodeEnum.PARAM_NOT_VALID.getErrorCode();
+        BindingResult bindingResult = e.getBindingResult();
+        StringBuilder errorMessage = new StringBuilder();
+        Optional.ofNullable(bindingResult.getFieldErrors()).ifPresent(errors ->
+                errors.forEach(error ->
+                        errorMessage.append(error.getField())
+                                .append(" ")
+                                .append(error.getDefaultMessage())
+                                .append(", 当前值: '")
+                                .append(error.getRejectedValue())
+                                .append("'; ")));
+        log.warn("{} request error, errorCode: {}, errorMessage: {}", request.getRequestURI(), errorCode, errorMessage);
+        return Response.fail(errorCode, errorMessage.toString());
+    }
+
+    /**
+     * 捕获 guava 参数校验异常
+     *
+     * @return
+     */
+    @ExceptionHandler({IllegalArgumentException.class})
+    @ResponseBody
+    public Response<Object> handleIllegalArgumentException(HttpServletRequest request, IllegalArgumentException e) {
+        String errorCode = ResponseCodeEnum.PARAM_NOT_VALID.getErrorCode();
+        String errorMessage = e.getMessage();
+        log.warn("{} request error, errorCode: {}, errorMessage: {}", request.getRequestURI(), errorCode, errorMessage);
+        return Response.fail(errorCode, errorMessage);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Response<Object> handleException(HttpServletRequest request, Exception e) {
+        log.error("{} request error", request.getRequestURI(), e);
+        return Response.fail(ResponseCodeEnum.SYSTEM_ERROR);
+    }
+}
