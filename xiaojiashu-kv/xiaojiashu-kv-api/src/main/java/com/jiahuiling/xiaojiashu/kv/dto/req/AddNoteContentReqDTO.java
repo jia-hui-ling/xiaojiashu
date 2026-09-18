3aee609cd892dba332017093e7b156bf49231831
scout16 <scout16@noreply.gitcode.com>
2026-09-18 10:15:34 +0800

    K-V 键值服务改造

--- xiaojiashu-kv/xiaojiashu-kv-api/src/main/java/com/jiahuiling/xiaojiashu/kv/dto/req/AddNoteContentReqDTO.java
+++ xiaojiashu-kv/xiaojiashu-kv-api/src/main/java/com/jiahuiling/xiaojiashu/kv/dto/req/AddNoteContentReqDTO.java
@@ -19,8 +19,8 @@ import lombok.NoArgsConstructor;
 @Builder
 public class AddNoteContentReqDTO {
 
-    @NotNull(message = "笔记 ID 不能为空")
-    private Long noteId;
+    @NotNull(message = "笔记内容 uuid 不能为空")
+    private String uuid;
 
     @NotBlank(message = "笔记内容不能为空")
     private String content;
